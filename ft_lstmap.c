396714bf2a28134938c0c772f10218adf20c7d52
Mohammed Musthafa Mohammed Munshid <mmohamm2@c2r1s2.42london.com>
2025-11-17 15:58:00 +0000

    polished

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmohamm2 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:11:50 by mmohamm2          #+#    #+#             */
+/*   Updated: 2025/11/17 15:12:36 by mmohamm2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdlib.h>
 
-void	ft_nodedel(t_list **list,void *content, void (*del)(void *))
+void	ft_nodedel(t_list **list, void *content, void (*del)(void *))
 {
 	del(content);
 	ft_lstclear(list, del);
 }
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+
+t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *))
 {
 	t_list	*list;
 	t_list	*node;
@@ -26,7 +39,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		node = ft_lstnew(temp_content);
 		if (!node)
 		{
-			ft_nodedel(&list,temp_content, del);
+			ft_nodedel(&list, temp_content, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&list, node);
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmohamm2 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:13:18 by mmohamm2          #+#    #+#             */
+/*   Updated: 2025/11/17 15:13:53 by mmohamm2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_lstsize(t_list *lst)
 {
-	t_list *current;
-	int	size;
-	
+	t_list	*current;
+	int		size;
+
 	size = 0;
 	current = lst;
 	while (current)
 	{
 		size++;
 		current = current->next;
-	}	
+	}
 	return (size);
 }
